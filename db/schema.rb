9cc1c5d3ee8f120462c3e2c4269cae4b5bfd32f8
Gilad Kleinman <dkgs1998@gmail.com>
2015-08-14 05:42:14 +0300

    added balance to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814011218) do
+ActiveRecord::Schema.define(version: 20150814024115) do
 
   create_table "bounties", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150814011218) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.float    "balance"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
