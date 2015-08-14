0e1a1fd629b500c0c47025b1dfeaaea04f662e43
Gilad Kleinman <dkgs1998@gmail.com>
2015-08-15 02:17:53 +0300

    added more

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,8 @@ class Answer < ActiveRecord::Base
 	validate :bounty_answer_available
 
 	after_create :change_bounty_status
+
+	scope :pending_message, -> (id) {find_by_sql(["SELECT * from Answers a JOIN bounty_hunters b ON a.user_id=b.user_id WHERE b.status=1 AND b.bounty_id=?", [id]]).first}
 	def bounty_answer_available
 	  if !self.bounty.open?
         errors.add(:bounty_id, "is not valid, bounty is not open for answers")
@@ -13,4 +15,5 @@ class Answer < ActiveRecord::Base
 	def change_bounty_status
 		self.bounty.pending!
 	end
+	#SELECT answer_id FROM answers a JOIN bounty_hunters b ON a.user_id = b.user_id WHERE b.status = 1 AND b.bounty_id = 2
 end
