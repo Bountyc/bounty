a92c9908910c062ef5ab7b2504b8367c889ac36c
Gilad Kleinman <dkgs1998@gmail.com>
2015-08-15 21:57:07 +0300

    this is gettin gannoying

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Bountyapp
       "<div class=\"has-error\">#{html_tag}</div>".html_safe
     }
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.assets.enabled = true
   end
 end
