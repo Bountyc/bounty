928cb9c5c27304272fc68f942659789b554785a4
Gilad Kleinman <dkgs1998@gmail.com>
2015-08-21 01:49:51 +0300

    minor changes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,9 @@
 class TagsController < ApplicationController
 	def index
 		@tags = Tag.all
-		render json: Tag.where('name like ?', "%#{params[:q]}%").map {|tag| {id: tag.name}}
+		respond_to do |format|
+		    format.html
+		    format.json { render json: Tag.where('name like ?', "%#{params[:q]}%").map {|tag| {id: tag.name}} }
+		  end
 	end
 end
