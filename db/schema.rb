ee3846e5b12a2f10f662991f52dcea70c6f381ff
Maor Kern <maor.kern@gmail.com>
2015-08-20 18:19:07 +0300

    schema conflict for sure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815113313) do
+ActiveRecord::Schema.define(version: 20150816171608) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "description"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20150815113313) do
     t.boolean  "seen",              default: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.string   "action_link"
   end
 
   create_table "payments", force: :cascade do |t|
