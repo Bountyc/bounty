deb0362d6f9efd15463c00846a75674d7dbe60bb
Gilad Kleinman <dkgs1998@gmail.com>
2015-08-22 22:05:46 +0300

    only showing open bounties on sidebar now

--- app/helpers/bounties_helper.rb
+++ app/helpers/bounties_helper.rb
@@ -1,12 +1,12 @@
 module BountiesHelper
 	def load_highest_paying_bounties
 		# Using reorder because in model defined default scope to order by updated_at
-		@highest_paying_bounties = Bounty.limit(10).reorder("price DESC")
+		@highest_paying_bounties = Bounty.open_bounties.limit(10).reorder("price DESC")
 	end
 
 	def load_highest_viewed_bounties
 		# Using reorder because in model defined default scope to order by updated_at
-		@highest_viewed_bounties = Bounty.limit(10).select("bounties.*, COUNT(views.id) view_count").joins(:views).group("bounties.id").reorder("view_count DESC")
+		@highest_viewed_bounties = Bounty.open_bounties.limit(10).select("bounties.*, COUNT(views.id) view_count").joins(:views).group("bounties.id").reorder("view_count DESC")
 	end
 
 end
--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -17,6 +17,8 @@ class Bounty < ActiveRecord::Base
 	validate :poster_can_afford
 
 	scope :search, -> (field, text) { where("#{field} LIKE ?", "%#{text}%") }
+	scope :open_bounties, -> { where(status: 0) }
+
 	default_scope {order('updated_at DESC')}
 	def working_users
 		return User.joins(:bounty_hunters).where("bounty_hunters.status = 0").where("bounty_hunters.bounty_id = ?", [self.id])
