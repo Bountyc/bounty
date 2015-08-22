bd1c785106ec7152b84bf09f610ab31e2c683473
Gilad Kleinman <dkgs1998@gmail.com>
2015-08-22 23:37:13 +0300

    made notifications better

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,7 @@ class Notification < ActiveRecord::Base
 	belongs_to :bounty_hunter
 	belongs_to :user
 
-	enum notification_type: [:answer_denied, :answer_accepted, :new_answer]
+	enum notification_type: [:answer_denied, :answer_accepted, :new_answer, :started_working]
+	default_scope {order('updated_at DESC')}
+
 end
