d3f33d22b64cecdf7a5832d4a46e287c4f504d56
Gilad Kleinman <dkgs1998@gmail.com>
2015-08-23 16:32:33 +0300

    changed notifications

--- lib/html_generator.rb
+++ lib/html_generator.rb
@@ -5,10 +5,14 @@ module HtmlGenerator
 
 	include ActionView::Context
 
-	def notification_html(message, created_at)
+	def notification_html(message, created_at, seen)
 		content_tag :li, class: "strong" do
 			content_tag :a, href: "#" do
-				concat content_tag :span, message
+				if seen
+					concat content_tag :span, message
+				else
+					concat content_tag :span, message, class: "unseen-notification"
+				end
 				concat content_tag :h5, "- " + time_ago_in_words(created_at) + " ago"
 	 		end
 		end
