894d975ea869ca1156b82bae2b3f0fdbeaf3b2ba
Maor Kern <maor.kern@gmail.com>
2015-09-13 08:17:19 +0300

    migrated motherfucker

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,8 @@ class User < ActiveRecord::Base
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 	    user.email = auth.info.email
 	    user.password = Devise.friendly_token[0,20]
+	    user.first_name = "X"
+	    user.last_name = "X"
 	    user.save
 	  end
 	end
