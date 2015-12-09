9e85ddf4c6ab1ba206369f4181a474e28143d03e
Your Name <dkgs1998@gmail.com>
2015-12-09 22:27:35 +0200

    changed email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://young-shore-8277.herokuapp.com/' }
-  
+  config.action_mailer.default_url_options = { host: 'https://bountyc.com/' }
+
   config.force_ssl = false
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@bountyapp.con'
+  config.mailer_sender = 'noreply@bountyc.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
