fa5847d8118bfd37e18e002ae89ceec963a46332
Your Name <dkgs1998@gmail.com>
2015-12-10 03:20:47 +0200

    did withdrawals

--- app/mailers/admin_notifier.rb
+++ app/mailers/admin_notifier.rb
@@ -1,2 +1,12 @@
 class AdminNotifier < ApplicationMailer
+
+	def withdraw_money_admin_email(user, withdrawal)
+
+	@user = user
+	@withdrawal = withdrawal
+
+	mail( :to => ["dkgs1998@gmail.com", "maor.kern@gmail.com"],
+		:subject => 'Someone has asked to withdraw money!' )
+	end
+
 end
