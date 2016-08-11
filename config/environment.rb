3daa5f483069457bf13bc4143da95117332c5374
Maor Kern <maor.kern@gmail.com>
2016-08-11 12:09:00 +0300

    facebook app id/secret

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,7 @@ PUSHER_SECRET = '82869945fb73a18029c9'
 FACEBOOK_APP_ID = '280454752320340'
 FACEBOOK_SECRET = 'c7759b48c7c0e96f1772b2e7e4ce7ed1'
 
+
 GOOGLE_APP_ID = '265897584492-frucuiejdtu2peri8e8avf872ovv4lej.apps.googleusercontent.com'
 GOOGLE_APP_SECRET = 'PLkDYpqywDpeWxcS8Qki6gS3'
 # Initialize the Rails application.
